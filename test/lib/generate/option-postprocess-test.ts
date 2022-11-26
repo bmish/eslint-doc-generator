e2f9e72e5323a90e9d41d0a48fb9282e203892e1
Gareth Jones <jones258@gmail.com>
2022-11-27 11:54:58 +1300

    test: add cases for `postprocess`

---
+++ test/lib/generate/option-postprocess-test.ts
@@ -0,0 +1,60 @@
+import { generate } from '../../../lib/generator.js';
+import mockFs from 'mock-fs';
+import { dirname, resolve, relative } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { readFileSync } from 'node:fs';
+import { jest } from '@jest/globals';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const PATH_NODE_MODULES = resolve(__dirname, '..', '..', '..', 'node_modules');
+
+describe('generate (--postprocess)', function () {
+  describe('basic', function () {
+    beforeEach(function () {
+      mockFs({
+        'package.json': JSON.stringify({
+          name: 'eslint-plugin-test',
+          exports: 'index.js',
+          type: 'module',
+        }),
+
+        'index.js': `
+              export default {
+                rules: {
+                  'no-foo': {
+                    meta: {
+                      docs: { description: 'Description for no-foo.' },
+                      hasSuggestions: true,
+                      fixable: 'code',
+                      deprecated: true,
+                    },
+                    create(context) {}
+                  },
+                },
+              };`,
+
+        'README.md': '## Rules\n',
+
+        'docs/rules/no-foo.md': '',
+
+        // Needed for some of the test infrastructure to work.
+        node_modules: mockFs.load(PATH_NODE_MODULES),
+      });
+    });
+
+    afterEach(function () {
+      mockFs.restore();
+      jest.resetModules();
+    });
+
+    it('calls the postprocessor', async function () {
+      await generate('.', {
+        postprocess: (content, path) =>
+          [content, '', `Located at ${relative('.', path)}`].join('\n'),
+      });
+      expect(readFileSync('README.md', 'utf8')).toMatchSnapshot();
+      expect(readFileSync('docs/rules/no-foo.md', 'utf8')).toMatchSnapshot();
+    });
+  });
+});
