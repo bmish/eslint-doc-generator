5c34eedef0793d5c146a0d4906f4473aa1aef01d
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2022-12-19 11:46:47 -0500

    Ensure `deprecated`/`schema` properties are detected from function-style rules (#357)

--- test/lib/generate/rule-metadata-test.ts
+++ test/lib/generate/rule-metadata-test.ts
@@ -54,6 +54,60 @@ describe('generate (rule metadata)', function () {
     });
   });
 
+  describe('deprecated function-style rule with deprecated/schema properties', function () {
+    beforeEach(function () {
+      mockFs({
+        'package.json': JSON.stringify({
+          name: 'eslint-plugin-test',
+          exports: 'index.js',
+          type: 'module',
+        }),
+
+        'index.js': `
+          const noFoo = function create () {};
+          noFoo.deprecated = true;
+          noFoo.schema = [
+            {
+              type: 'object',
+              properties: {
+                optionToDoSomething: {
+                  type: 'boolean',
+                  default: false,
+                },
+              },
+              additionalProperties: false,
+            },
+          ];
+          export default {
+            rules: {
+              'no-foo': noFoo
+            },
+          };`,
+
+        'README.md': '## Rules\n',
+
+        'docs/rules/no-foo.md': '## Options\noptionToDoSomething',
+
+        // Needed for some of the test infrastructure to work.
+        node_modules: mockFs.load(PATH_NODE_MODULES),
+      });
+    });
+
+    afterEach(function () {
+      mockFs.restore();
+      jest.resetModules();
+    });
+
+    it('generates the documentation', async function () {
+      await generate('.', {
+        // Ensure the relevant properties are shown for the test.
+        ruleListColumns: ['name', 'deprecated', 'options'],
+      });
+      expect(readFileSync('README.md', 'utf8')).toMatchSnapshot();
+      expect(readFileSync('docs/rules/no-foo.md', 'utf8')).toMatchSnapshot();
+    });
+  });
+
   describe('rule with no meta object', function () {
     beforeEach(function () {
       mockFs({
