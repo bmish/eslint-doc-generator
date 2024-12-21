b04992dc4983941148fd1eab505d817b43053608
Hasegawa-Yukihiro <y.h.baskeeee@icloud.com>
2024-12-21 12:31:37 +0900

    test: Add tests for boolean function

--- test/lib/boolean-test.ts
+++ test/lib/boolean-test.ts
@@ -1,6 +1,30 @@
-import { isBooleanable } from '../../lib/boolean.js';
+import { boolean, isBooleanable } from '../../lib/boolean.js';
 
 describe('boolean', function () {
+  describe('#boolean', function () {
+    it.each(['true', 't', 'yes', 'y', 'on', '1'])(
+      'returns true when string is %s',
+      function (value) {
+        expect(boolean(value)).toBe(true);
+      },
+    );
+
+    it('returns true when number is 1', function () {
+      expect(boolean(1)).toBe(true);
+    });
+
+    it('returns true when boolean is true', function () {
+      expect(boolean(true)).toBe(true);
+    });
+
+    it.each(['foo', 2, undefined])(
+      'returns false when value is %p',
+      function (value) {
+        expect(boolean(value)).toBe(false);
+      },
+    );
+  });
+
   describe('#isBooleanable', function () {
     it.each([
       'true',
